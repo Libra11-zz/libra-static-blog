87e5f882f71ba09c295b5d32184087710322ce59
Libra <libra_085925@163.com>
2021-04-06 09:36:02 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,6 +12,8 @@ module.exports = {
       },
     ],
   ],
+  // 文章目录和 back to top
+  plugins: ["vuepress-plugin-table-of-contents", "@vuepress/back-to-top"],
   // theme:
   themeConfig: {
     // 主页
@@ -38,6 +40,9 @@ module.exports = {
       title: "Hello There !!!!",
       subTitle1: "Thank you for visiting my blog",
       subTitle2: "Hope we can make progress together",
+      // QQ二维码地址
+      qq:
+        "https://libra321.oss-cn-huhehaote.aliyuncs.com/blog/weixin-qrcode.png",
       // 微信二维码地址
       wechat:
         "https://libra321.oss-cn-huhehaote.aliyuncs.com/blog/weixin-qrcode.png",
